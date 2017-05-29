a6b943d7bef1c103e6e7220565fadaf48e700d9d
= <=>
2017-05-29 17:48:38 +0800

    douzuiwa project

---
+++ src/main/java/com/douzuiwa/dao/OrderItemMapper.java
@@ -0,0 +1,18 @@
+package com.douzuiwa.dao;
+
+import com.douzuiwa.pojo.OrderItem;
+
+public interface OrderItemMapper {
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/douzuiwa/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.douzuiwa.dao;
+
+import com.douzuiwa.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
